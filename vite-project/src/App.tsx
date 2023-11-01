a1191cac4c8b261c7139fb9a0ed264786b645e52
Jae Kang <jae.kang@wsp.com>
2023-10-31 18:09:34 -0600

    daily upload; no significant progress

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,38 +6,63 @@ import imageURL from "./assets/Difficult.jpeg";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [clickLocation, setClickLocation] = useState<number[]>([0, 0]);
+  const isFirstClickDone = useRef(false);
+  const isSelected = useState(false);
+  const [boxes, setBoxes] = useState([]);
+  
+  const handleClick = ({ pageX, pageY }) => {
+    // on every click push a new coordinate to the boxes array
+    setBoxes((boxes) => [...boxes, { x: pageX, y: pageY }]);
+  };
 
-  function handleWheel(e) {
-    if (e.deltaY > 0) {
-      zoomElement.style.transform = `scale(${(zoom += ZOOM_SPEED)})`;
-    } else {
-      zoomElement.style.transform = `scale(${(zoom -= ZOOM_SPEED)})`;
-    }
-  });
+  return (
+    <div className="app" onClick={handleClick}>
+      // display boxes
+      {boxes.map((box) => (
+        // map coordinates to left and top
+        <div className="box" style={{ left: box.x, top: box.y }}></div>
+      ))}
+    </div>
+  );
+}
 
   return (
     <>
-      <img src={imageURL} alt="img"   onWheel = {(e) => handleWheel(e)}  />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div
+        id="main"
+        onClick={(e) => {
+          if (document.getElementsByClassName("selected").length == 0) {
+            setClickLocation([e.pageX, e.pageY]);
+            addSelectedMarker(clickLocation);
+            isFirstClickDone.current = true;
+          } else {
+            removeSelectedMarker();
+          }
+        }}
+      >
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
+        <img src={imageURL} alt="img" id="mainImage" className="workspace" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
